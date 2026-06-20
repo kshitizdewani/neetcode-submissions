c6ce6283cbbdee58abad142006e3603866de07ee
Kshitiz Dewani <58976268+kshitizdewani@users.noreply.github.com>
2026-06-20 17:38:55 +0530

    Add: majority-element - submission-10

---
+++ Data Structures & Algorithms/majority-element/submission-10.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        sorted_nums = sorted(nums)
+        middle = math.ceil(len(nums)/2)
+
+        half1_set = set(sorted_nums[:middle])
+        half2_set = set(sorted_nums[middle:])
+
+        # print(middle, sorted_nums[:middle], sorted_nums[middle:])
+
+        for item in half2_set:
+            if item in half1_set:
+                return item
+        return list(half1_set)[0]
